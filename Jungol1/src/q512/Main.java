088fc4c46d31e0f29838cc35357f9ddd34842875
Administrator <Administrator@User-2022ILFYWR>
2022-11-02 17:44:37 +0900

    문제 풀이

---
+++ Jungol1/src/q512/Main.java
@@ -0,0 +1,11 @@
+package q512;
+
+public class Main {
+	public static void main(String[] args) {
+		int weight = 49;
+		double ratio = 0.2683;
+
+//		System.out.println(weight + " * " + ratio + " = " + (weight * ratio));
+		System.out.printf("%d * %f = %f\n", weight, ratio, weight * ratio);
+	}
+}
