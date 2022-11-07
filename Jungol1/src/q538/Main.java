020a53b8a38d39c2c582099f22061811ac17629e
Administrator <Administrator@User-2022ILFYWR>
2022-11-07 10:40:32 +0900

    .

--- Jungol1/src/q538/Main.java
+++ Jungol1/src/q538/Main.java
@@ -4,19 +4,17 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("numder?");
 		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		sc.close();
-
-//	      System.out.println(num);
 
 		for (;;) {
+			System.out.print("number? ");
+			int num = sc.nextInt();
 			if (0 < num) {
 				System.out.println("positive integer");
-			} else if (num > 0) {
+			} else if (num < 0) {
 				System.out.println("negative number");
 			} else {
+				sc.close();
 				break;
 			}
 		}
