30f81d5db1a6626ad4926be54ea72052b1760421
Administrator <Administrator@User-2022ILFYWR>
2022-11-16 17:25:14 +0900

    613

--- Jungol1/16struct/q613/Main.java
+++ Jungol1/16struct/q613/Main.java
@@ -1,10 +1,38 @@
 package q613;
 
-public class Main {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+class Student {
+	private String name;
+	private String school;
+	private int grade;
 
+	public Student(String name, String school, int grade) {
+		this.name = name;
+		this.school = school;
+		this.grade = grade;
 	}
 
+	public void print() {
+		System.out.println("Name : " + name);
+		System.out.println("School : " + school);
+		System.out.println("Grade : " + grade);
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		String school = sc.next();
+		int grade = sc.nextInt();
+		sc.close();
+
+//		System.out.println(name);
+//		System.out.println(school);
+//		System.out.println(grade);
+		
+		Student stu = new Student(name, school, grade);
+		stu.print();
+	}
 }
