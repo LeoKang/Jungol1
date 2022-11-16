fec817b20a4d8bdd8d48da8a484603120b056c54
Administrator <Administrator@User-2022ILFYWR>
2022-11-16 15:45:35 +0900

    596

---
+++ Jungol1/string1/q596/Main.java
@@ -0,0 +1,19 @@
+package q596;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String inp = sc.next();
+		int n = sc.nextInt();
+		sc.close();
+
+//		System.out.println(inp);
+//		System.out.println(n);
+
+		for (int i = inp.length() - 1; n > 0; i--, n--) {
+			System.out.print(inp.charAt(i));
+		}
+	}
+}
