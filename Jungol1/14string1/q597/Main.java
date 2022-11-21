bcc1fb49e998c354a741d06990fa7f5d4a68d654
Administrator <Administrator@User-2022ILFYWR>
2022-11-21 17:24:03 +0900

    597

---
+++ Jungol1/14string1/q597/Main.java
@@ -0,0 +1,14 @@
+package q597;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str1 = sc.next();
+		String str2 = sc.next();
+		sc.close();
+
+		System.out.println(str1.length() + str2.length());
+	}
+}
