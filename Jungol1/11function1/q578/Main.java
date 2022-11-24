3d40f5f903e566b14564b1727daa52f83a6b1838
Administrator <Administrator@User-2022ILFYWR>
2022-11-24 10:18:10 +0900

    578-2

--- Jungol1/11function1/q578/Main.java
+++ Jungol1/11function1/q578/Main.java
@@ -2,24 +2,29 @@ package q578;
 
 import java.util.Scanner;
 
-public class Main {
-	public static void main(String[] args) {
+class Gugudan {
+	private int inp1;
+	private int inp2;
+
+	public void input() {
 		Scanner sc = new Scanner(System.in);
-		int inp1 = sc.nextInt();
-		int inp2 = sc.nextInt();
+		inp1 = sc.nextInt();
+		inp2 = sc.nextInt();
 		sc.close();
 
 //		System.out.println(inp1 + " " + inp2);
+	}
 
+	public void swap() {
 		// 1. swap
 		if (inp1 > inp2) {
 			int tmp = inp1;
 			inp1 = inp2;
 			inp2 = tmp;
 		}
+	}
 
-//		System.out.println(inp1 + " " + inp2);
-
+	public void print() {
 		// 2. print
 		for (int i = inp1; i <= inp2; i++) {
 			System.out.println("== " + i + "dan ==");
@@ -29,5 +34,14 @@ public class Main {
 			}
 			System.out.println();
 		}
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Gugudan gu = new Gugudan();
+		gu.input();
+		gu.swap();
+		gu.print();
 	}
 }
