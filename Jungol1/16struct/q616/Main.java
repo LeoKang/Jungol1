c87912ada3a4fef5009f16ded11d859918037ce7
Administrator <Administrator@User-2022ILFYWR>
2022-12-01 12:37:54 +0900

    616

--- Jungol1/16struct/q616/Main.java
+++ Jungol1/16struct/q616/Main.java
@@ -1,15 +1,69 @@
 package q616;
 
+import java.util.Scanner;
+
 class Point {
-	int x, y;
+	private int x, y;
+
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public void print() {
+		System.out.println(x + " " + y);
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
 }
 
 class Triangle {
-	Point[] p;
+	private Point[] p;
+
+	public Triangle(int[] param) {
+		p = new Point[3];
+		for (int i = 0, j = 0; i < p.length; i++, j += 2) {
+			p[i] = new Point(param[j], param[j + 1]);
+		}
+	}
+
+	public void print() {
+		for (int i = 0; i < p.length; i++) {
+			p[i].print();
+		}
+	}
+
+	public void getCoG() {
+		int x = 0, y = 0;
+		for (int i = 0; i < p.length; i++) {
+			x += p[i].getX();
+			y += p[i].getY();
+		}
+		System.out.printf("(%.1f, %.1f)\n", (double) x / 3, (double) y / 3);
+	}
 }
 
 public class Main {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] p = new int[6];
+		for (int i = 0; i < p.length; i++) {
+			p[i] = sc.nextInt();
+		}
+		sc.close();
 
+//		for (int i = 0; i < p.length; i++) {
+//			System.out.print(p[i] + " ");
+//		}
+
+		Triangle tri = new Triangle(p);
+//		tri.print();
+		tri.getCoG();
 	}
 }
